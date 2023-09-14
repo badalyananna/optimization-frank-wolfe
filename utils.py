18b549dd4f93f5d26daca6783a5a5c36a7dcc280
Anna Badalyan <abadalyan@sg-macbook-02.is.localnet>
2023-09-14 17:10:02 +0200

    Added the plottinf function, renamed the preprocessing to utils

--- utils.py
+++ utils.py
@@ -1,7 +1,10 @@
 from itertools import combinations
-from typing import List, Tuple
+from typing import List, Tuple, Optional
 
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 def process_dataset(path: str, k: int) -> Tuple[int, List[List], List[List]]:
     """
@@ -55,3 +58,37 @@ def process_dataset(path: str, k: int) -> Tuple[int, List[List], List[List]]:
             else:
                 complement_hyperedges.append(comb)
         return n, hyperedges, complement_hyperedges
+    
+def plot_hisotry(df: pd.DataFrame, seed: int, ssc: Optional[bool]=False):
+    """The function to plot the history of the specific run of the algorithm based on seed.
+    The plots the ssc=True plot the dashed line for the run with SSC procedure."""
+    
+    fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(8, 8), sharex='col', sharey='row')
+    dfs = df[df.seed==seed]
+    assert len(dfs) > 0, "The dataframe is empty. Probably the seed doesn't exist"
+    for i in range(2):
+        for j in range(2):
+            ax = axes[j, i]
+            x = 'cpu_time' if i == 0 else 'iteration'
+            y = 'of_value' if j == 0 else 'duality_gap'
+            x_name = 'CPU time' if i == 0 else 'Iterations'
+            y_name = 'O.F. value' if j == 0 else 'Duality Gap'
+            if ssc:
+                sns.lineplot(data=dfs, x=x, y=y, hue='Variant', style='SSC', ax=ax)
+            else:
+                sns.lineplot(data=dfs, x=x, y=y, hue='Variant', ax=ax)
+            ax.set_yscale('log')
+            ax.spines[['right', 'top']].set_visible(False)
+            handles, labels = ax.get_legend_handles_labels()
+            ax.get_legend().remove()
+            ax.set_ylabel(y_name)
+            ax.set_xlabel(x_name)
+    if ssc:
+        fig.legend(handles, labels, loc="center left", bbox_to_anchor=(1.0, 0.5), frameon=False)
+        fig.suptitle('Performance of FW variants with and without SSC procedure', fontsize=14, y=1)
+    else:
+        fig.legend(handles, labels, title='Variants', loc="center left", bbox_to_anchor=(1.0, 0.5), frameon=False)
+        fig.suptitle('Performance of FW variants', fontsize=14, y=1)
+
+    plt.tight_layout()  # Adjust spacing between subplots
+    plt.show()
